0f9498f297c4c583e14fa709d76d3dff2ab34680
Phoebe Alexa Yumul <112922385+phoebealexa27@users.noreply.github.com>
2022-09-13 00:23:29 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+
+let http = new XMLHttpRequest();
+
+http.open('get', 'products.json', true);
+
+
+
+http.send();
+
+http.onload = function(){
+	
+	if(this.readyState == 4 && this.status == 200){
+		
+		let products = JSON.parse(this.responseText);
+
+
+		let output = "";
+
+		for(let item of products){
+			output += `
+				<div class="product">
+					<img src="${item.image}" alt="${item.description}">
+					<p class="title">${item.title}</p>
+					<p class="price">
+						<span>${item.price}</span>
+					</p>
+					<p class="description">${item.description}</p>
+					<p class="cart">Add to cart <i class="bx bx-cart-alt"></i></p>
+				</div>
+			`;
+		}
+		
+		document.querySelector(".products").innerHTML = output;
+	}
+} 
